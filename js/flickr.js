65eb22a4c9f3fdbd825ceee71b8fb3c187ea73ed
mania605 <kim.minsun83@gmail.com>
2024-09-25 14:14:29 +0900

    flickr data fething후 동적으로 이미지 제목 출력

--- js/flickr.js
+++ js/flickr.js
@@ -2,7 +2,7 @@ const api_key = "d0053a4bfac353553d2d0337fd052214";
 const baseURL = `https://www.flickr.com/services/rest/?api_key=${api_key}&method=`;
 const myID = "201491599@N03";
 const method_mine = "flickr.people.getPhotos";
-let url = "${baseURL}${method_mine}&user_id=${myID}&nojsoncallback=1&format=json";
+let url = `${baseURL}${method_mine}&user_id=${myID}&nojsoncallback=1&format=json`;
 //기본요청URL에 쿼리값 제대로 넣어서 fetch처리했을때
 //콘솔문에 not valid json 오류뜨는 경우
 //원인 : json데이터가 callback함수안에 들어가 있는 경우
@@ -11,5 +11,17 @@ let url = "${baseURL}${method_mine}&user_id=${myID}&nojsoncallback=1&format=json
 fetch(url)
   .then((data) => data.json())
   .then((json) => {
-    console.log(json);
+    console.log(json.photos.photo);
+    const picArr = json.photos.photo;
+    let tags = '';
+
+    picArr.forEach((pic) => {
+      tags += `
+        <li>
+          <h2>${pic.title}</h2>
+        </li>
+      `;
+    });
+
+    list.innerHTML = tags;
   });
