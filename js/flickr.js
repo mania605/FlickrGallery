376622e674653f8b0dbd1f2cd538b5fef8fe9fcd
mania605 <kim.minsun83@gmail.com>
2024-09-26 11:57:14 +0900

    검색 갤러리 기능 추가

--- js/flickr.js
+++ js/flickr.js
@@ -1,5 +1,8 @@
-const [btnMine, btnPopular] = document.querySelectorAll("nav button");
 let dataType = "";
+const [btnMine, btnPopular] = document.querySelectorAll("nav button");
+//searchBox안쪽에 있는 두번째 요소인 input, 세번째 요소인 btnSearch를 비구조할당으로 변수할당
+const [_, inputSearch, btnSearch] = document.querySelector(".searchBox").children;
+console.log(inputSearch);
 
 //스크립트 처음 로드시에는 내갤러리 출력
 ///fetch함수 호출시 인수값을 객체 형태로 전달
@@ -9,14 +12,20 @@ fetchFlickr({ type: "mine" });
 //각 버튼 클릭시 갤러리 타입 변경
 btnMine.addEventListener("click", () => fetchFlickr({ type: "mine" }));
 btnPopular.addEventListener("click", () => fetchFlickr({ type: "interest" }));
-
+//검색 버튼 클릭시
+btnSearch.addEventListener("click", () => {
+  //인풋요소의 value값 (검색어)을 tags에 담아 fetchFlickr함수 호출
+  fetchFlickr({ type: "search", tags: inputSearch.value });
+  //호출시 input요소의 검색어는 지워줌
+  inputSearch.value = "";
+});
 //특정 요소에 특정 함수 연결
 document.body.addEventListener("click", (e) => {
   if (e.target.className === "thumb") createModal(e);
   if (e.target.className === "btnClose") removeModal();
   //목록에서 사용자 프로필 아이디 클릭시 클릭한 Span요소의 사용자 아이디 텍스트값을 fetchFlickr의 인수로 전달해 호출
-  if (e.target.className === "userID")
-    fetchFlickr({ type: "user", userID: e.target.innerText }); //user타입 갤러리에는 useID라는 추가 프로퍼티로 유저아이디값을 전달
+  if (e.target.className === "userID") fetchFlickr({ type: "user", userID: e.target.innerText });
+  //user타입 갤러리에는 useID라는 추가 프로퍼티로 유저아이디값을 전달
 });
 
 //flickr fetching함수
@@ -33,16 +42,20 @@ function fetchFlickr(opt) { //type이 opt라는 파라미터 객체안쪽에 들
   const myID = "201491599@N03";
   const method_mine = "flickr.people.getPhotos";
   const method_interest = "flickr.interestingness.getList";
+  const method_search = "flickr.photos.search";
   let url_mine = `${baseURL}${method_mine}&user_id=${myID}&nojsoncallback=1&format=json`;
   //type으로 특정사용자의 ID값을 전달 받은뒤 user_id쿼리에 연결하면 해당 사용자의 갤러리를 호출하는 요청 URL생성
   let url_user = `${baseURL}${method_mine}&user_id=${opt.userID}&nojsoncallback=1&format=json`;
   let url_interest = `${baseURL}${method_interest}&nojsoncallback=1&format=json`;
+  //opt로 전달된 tag프로퍼티의 값을 tag라는 쿼리값연동
+  let url_search = `${baseURL}${method_search}&tags=${opt.tags}&nojsoncallback=1&format=json`;
   let result_url = "";
   if (opt.type === "mine") result_url = url_mine;
-  else if (opt.type === "interest") result_url = url_interest;
+  if (opt.type === "interest") result_url = url_interest;
   //type이 mine, interest가 아니면 특정 사용자 ID일테니 url_user요청 아이디를 result_url에 담아서 fetch함수에 전달
-  else if (opt.type === "user") result_url = url_user;
-
+  if (opt.type === "user") result_url = url_user;
+  //전달된 opt.type값이 search이면 url_search 요청을 fetch함수에 전달
+  if (opt.type === "search") result_url = url_search;
 
   fetch(result_url)
     .then((data) => data.json())
@@ -57,21 +70,15 @@ function createList(dataArr) {
   const list = document.querySelector(".list");
   let tags = "";
 
-  dataArr.forEach((pic) => {
+  dataArr.forEach(pic => {
     tags += `
-    <li >
+        <li>
           <figure class='pic'>
             <img class='thumb' src="https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_z.jpg" alt="https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_b.jpg" />
-          </figure>
-          <h2>${pic.title}</h2>
-
-          <div class='profile'>
-            <img src='http://farm${pic.farm}.staticflickr.com/${pic.server}/buddyicons/${pic.owner}.jpg' alt=${pic.owner} /> <span class='userID'>${pic.owner}</span>
           </div>
         </li>
-    `;
+      `;
   });
-
   list.innerHTML = tags;
 
   setDefImg();
@@ -87,9 +94,7 @@ function setDefImg() {
     (imgEl.onerror = () =>
       imgEl.setAttribute(
         "src",
-        "https://www.flickr.com/images/buddyicon.gif"
-      ))
-  );
+        "https://www.flickr.com/images/buddyicon.gif")));
 }
 
 //모달생성 함수
