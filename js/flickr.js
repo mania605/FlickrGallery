12434039c341df0a763a8c0027aeeac852620fb5
mania605 <kim.minsun83@gmail.com>
2024-09-26 10:40:33 +0900

    특정사용자 아이디 클릭시 데이터호출

--- js/flickr.js
+++ js/flickr.js
@@ -12,6 +12,8 @@ btnPopular.addEventListener("click", () => fetchFlickr("interest"));
 document.body.addEventListener("click", (e) => {
   if (e.target.className === "thumb") createModal(e);
   if (e.target.className === "btnClose") removeModal();
+  //목록에서 사용자 프로필 아이디 클릭시 클릭한 Span요소의 사용자 아이디 텍스트값을 fetchFlickr의 인수로 전달해 호출
+  if (e.target.className === "userID") fetchFlickr(e.target.innerText);
 });
 
 //flickr fetching함수
@@ -23,15 +25,23 @@ function fetchFlickr(type) {
   //인수로 전달된 타입명으로 현재 dataType을 변경
   dataType = type;
 
-
   const api_key = "d0053a4bfac353553d2d0337fd052214";
   const baseURL = `https://www.flickr.com/services/rest/?api_key=${api_key}&method=`;
   const myID = "201491599@N03";
   const method_mine = "flickr.people.getPhotos";
   const method_interest = "flickr.interestingness.getList";
   let url_mine = `${baseURL}${method_mine}&user_id=${myID}&nojsoncallback=1&format=json`;
+  //type으로 특정사용자의 ID값을 전달 받은뒤 user_id쿼리에 연결하면 해당 사용자의 갤러리를 호출하는 요청 URL생성
+  let url_user = `${baseURL}${method_mine}&user_id=${type}&nojsoncallback=1&format=json`;
   let url_interest = `${baseURL}${method_interest}&nojsoncallback=1&format=json`;
-  let result_url = type === "mine" ? url_mine : url_interest;
+  let result_url = "";
+  if (type === "mine") result_url = url_mine;
+  else if (type === "interest") result_url = url_interest;
+  //type이 mine, interest가 아니면 특정 사용자 ID일테니 url_user요청 아이디를 result_url에 담아서 fetch함수에 전달
+  else result_url = url_user;
+
+
+
 
   fetch(result_url)
     .then((data) => data.json())
@@ -48,17 +58,17 @@ function createList(dataArr) {
 
   dataArr.forEach((pic) => {
     tags += `
-        <li>
+    <li >
           <figure class='pic'>
             <img class='thumb' src="https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_z.jpg" alt="https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_b.jpg" />
           </figure>
           <h2>${pic.title}</h2>
 
           <div class='profile'>
-            <img src='http://farm${pic.farm}.staticflickr.com/${pic.server}/buddyicons/${pic.owner}.jpg' alt=${pic.owner} /> <span>${pic.owner}</span>
+            <img src='http://farm${pic.farm}.staticflickr.com/${pic.server}/buddyicons/${pic.owner}.jpg' alt=${pic.owner} /> <span class='userID'>${pic.owner}</span>
           </div>
         </li>
-      `;
+    `;
   });
 
   list.innerHTML = tags;
@@ -87,11 +97,11 @@ function createModal(e) {
   const modal = document.createElement("aside");
   modal.classList.add("modal");
   modal.innerHTML = `
-      <div class='con'>
-        <img src=${imgSrc} />
-      </div>
-      <button class='btnClose'>CLOSE</button>
-    `;
+    < div class='con' >
+      <img src=${imgSrc} />
+      </ >
+    <button class='btnClose'>CLOSE</button>
+  `;
   document.body.append(modal);
 }
 
